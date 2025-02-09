7b386cd416a28b2a4f54f8c4f7f9045df511d35b
WillyamRodrigues <wrodrigues.eng@gmail.com>
2025-02-09 09:18:16 -0300

    first commit

---
+++ src/components/movies.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService"
+
+class Movies extends Component {
+    state = {
+        movies: getMovies()
+      } 
+
+    deleteMovie = (movie) => {
+        const  movies = this.state.movies.filter( m => m._id !== movie._id );
+        this.setState({movies})
+    }
+
+    render() {
+        const { length : count } = this.state.movies;
+
+        if (count === 0)
+            return <p>There are no movies in the database.</p>
+
+        return (
+            <div className = "p-5">
+                <p>Showing {count} movies in the database.</p>
+                <table className= "table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Genre</th>
+                            <th>Stock</th>
+                            <th>Rate</th>
+                            <th> </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.movies.map(movie => 
+                        <tr key={movie._id}>
+                            <td>{movie.title}</td>
+                            <td>{movie.genre.name}</td>
+                            <td>{movie.numberInStock}</td>
+                            <td>{movie.dailyRentalRate}</td>
+                            <td className="d-flex justify-content-center" onClick={() => this.deleteMovie(movie)}><button className="btn btn-danger center">Delete</button></td>
+                        </tr>)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+ 
+export default Movies;
+
+
+
